eb7ac6e38883744075b02eb584badd6c335677dd
carloschen1126 <carlos79103@gmail.com>
2021-12-18 00:43:36 +0800

    hw2 finish

--- 1101_CA_HW2/HW2.v
+++ 1101_CA_HW2/HW2.v
@@ -31,17 +31,19 @@ module ALU(
     reg  [63:0] shreg, shreg_nxt;
     reg  [31:0] alu_in, alu_in_nxt;
     reg  [32:0] alu_out;
-
+    reg ready_reg;
     // Todo: Instatiate any primitives if needed
 
     // Todo 5: Wire assignments
+    assign ready=ready_reg;
     assign out=shreg;
     // Combinational always block
     // Todo 1: Next-state logic of state machine
     always @(*) begin
         case(state)
             IDLE: begin
-                counter = 0;
+                //counter = 0;
+                ready_reg=0;
                 if(valid)
                     if(mode==0)
                         state_nxt = MUL;
@@ -51,41 +53,54 @@ module ALU(
                         state_nxt = AND;
                     else if(mode==3)
                         state_nxt = OR;
+                    else
+                        state_nxt = IDLE;
+                    
                 else
                     state_nxt = IDLE;
 
             end
             MUL : begin
-                if( counter != 31)
+                if( counter < 31)
                     begin
                         state_nxt = MUL;
+                        ready_reg=0;
+                        //ready_reg=0;
                     end
                 else
                     begin
                         state_nxt = OUT;
+                        ready_reg=0;
                     end
             end
             DIV : begin
-                if( counter != 31)
+                if( counter < 31)
                     begin
                         state_nxt = DIV;
+                        ready_reg=0;
+                        //ready_reg=0;
                     end
                 else
                     begin
                         state_nxt = OUT;
+                        ready_reg=0;
                     end
             end  
             AND : begin
                 state_nxt = OUT;
+                ready_reg=0;
             end
             OR  : begin
                 state_nxt = OUT;
+                ready_reg=0;
             end
             OUT : begin
                 state_nxt = IDLE;
+                ready_reg=1;
             end
             default : begin
                 state_nxt = IDLE;
+                ready_reg=0;
             end
         endcase
     end
@@ -98,7 +113,8 @@ module ALU(
             DIV: begin
                 counter_nxt = counter + 1;
             end
-            default: counter_nxt = 0; 
+            default: counter_nxt = 0;
+        endcase 
     end
 
 
@@ -118,61 +134,73 @@ module ALU(
     always @(*) begin
         case(state)
             MUL: begin
-                alu_out = shreg[63:32]+ alu_in;
+                alu_out = shreg[63:32]+ alu_in[31:0];
             end
             DIV : begin
-                alu_out = shreg[63:32]- alu_in;
-                
+                alu_out = shreg[62:31]- alu_in[31:0];
             end
             AND : begin
-                alu_out = shreg[31:0] ^ alu_in;
-                
+                alu_out = shreg[31:0] & alu_in[31:0];
             end
             OR : begin
-                alu_out = shreg[31:0] | alu_in;
+                alu_out = shreg[31:0] | alu_in[31:0];
             end
-            default: 
+            default:begin
+                alu_out = 0;
+            end
         endcase
     end
     // Todo 4: Shift register
     always @(*) begin
         case(state)
             IDLE: begin
+                shreg_nxt[63:32]=0;
                 if (valid) shreg_nxt[31:0] = in_A;
                 else       shreg_nxt[31:0] = 0;
             end
             MUL: begin
-                if(shreg[0]) begin 
-                    shreg_nxt[63:32] = alg_out;
-                    shreg_nxt[31:0] = shreg[31:0];
+                if(shreg[0]==1) begin
+                    //shreg[63]=0;
+                    shreg_nxt[63:31] = alu_out;
+                    shreg_nxt[30:0] = shreg[31:1];
                 end
                 else begin
-                    shreg_nxt = shreg;
+                    shreg_nxt = shreg >> 1;
                 end
-                shreg_nxt = shreg_nxt >> 1 ;
                 
             end
             DIV : begin
-                if(shreg[63:32] > alu_in) begin 
-                    shreg_nxt[63:32] = alg_out;
-                    shreg_nxt[31:0] = shreg[31:0];
-                    shreg_nxt = shreg_nxt << 1;
+                if(shreg[62:31] >= alu_in) begin 
+                    shreg_nxt[63:32] = alu_out;
+                    shreg_nxt[31:1] = shreg[30:0];
                     shreg_nxt[0]=1; 
                 end
                 else begin
-                    shreg_nxt = shreg;
-                    shreg_nxt = shreg_nxt << 1;
-                    shreg_nxt[0]=0; 
+                    shreg_nxt = shreg << 1;
+                    //shreg_nxt[0]=0; 
                 end
-                
             end
-            default: 
+            AND: begin
+                shreg_nxt[63:32]=0;
+                shreg_nxt[31:0]=alu_out[31:0];
+            end
+            OR: begin
+                shreg_nxt[63:32]=0;
+                shreg_nxt[31:0]=alu_out[31:0];
+            end
+            default: begin
+                shreg_nxt = 0;
+            end
+
         endcase
     end
     // Todo: Sequential always block
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             state <= IDLE;
+            alu_in <= 0;
+            counter <= 0;
+            shreg <= 0;
         end
         else begin
             state <= state_nxt;
